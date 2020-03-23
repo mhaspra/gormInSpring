87a027cc1667a4c149783b5149fbd627d0cd5786
haa <haa>
2020-03-23 17:22:12 +0100

    Added a simple spock integration test

---
+++ src/test/groovy/com/zuehlke/haa/gormInSpring/service/GraphicServiceISpec.groovy
@@ -0,0 +1,52 @@
+package com.zuehlke.haa.gormInSpring.service
+
+import com.zuehlke.haa.gormInSpring.domain.Graphic
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+import spock.lang.Specification
+
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles('test')
+class GraphicServiceISpec extends Specification{
+    @Autowired
+    GraphicService graphicService
+    @PersistenceContext
+    private EntityManager entityManager
+
+    void "graphic can be saved"() {
+        given:
+        Graphic graphic = new Graphic()
+        graphic.setAuthor("Mr. Spock");
+        graphic.setConfig("Test Config");
+
+        when:
+        graphicService.save(graphic);
+
+        then:
+        graphicService.findAll().size() == 1
+    }
+
+    void "graphic can be retrieved"() {
+        given:
+        Graphic graphic = new Graphic()
+        graphic.setAuthor("Mr. Spock");
+        graphic.setConfig("Test Config");
+        graphic.save()
+
+        and:
+        entityManager.clear()
+
+        when:
+        Graphic loadedGraphic = graphicService.get(graphic.getId());
+
+        then:
+        loadedGraphic.getAuthor() == graphic.getAuthor()
+        loadedGraphic.getConfig() == graphic.getConfig()
+    }
+}
